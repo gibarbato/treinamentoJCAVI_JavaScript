cc852b0bc48eec9bab4928824e31c0a7190925c4
gilberto barbato <gibarbato@gmail.com>
2022-08-22 22:50:01 -0300

    atualizando

--- Aula02/exercicios/exercicio03.js
+++ Aula02/exercicios/exercicio03.js
@@ -6,72 +6,72 @@
 - Estado Civil: 's', 'c', 'v', 'd';
 Dica: se sua variável é texto, o tamanho dela está armazenado em: texto.length */
 
-function validarUsuario(){
+function validarUsuario() {
 
     let nome = window.prompt(`
         Digite o Nome do usuário:
     `)
-        if (nome.length <= 3) {
-            alert(`digite um nome válido`)
-            validarUsuario()
-        } else {
-            alert('Nome válido')
-        }
-        
-        let idade = window.prompt(`
+    if (nome.length <= 3) {
+        alert(`digite um nome válido`)
+        validarUsuario()
+    } else {
+        alert('Nome válido')
+    }
+
+    let idade = window.prompt(`
         Digite a Idade do usuário:
     `)
-        if (idade >= 0 && idade <= 150) {
-            alert('Idade válida')
-        } else {
-            alert('Idade inválida')
-            validarUsuario()
-        }
-        
-        let salario = window.prompt(`
+    if (idade >= 0 && idade <= 150) {
+        alert('Idade válida')
+    } else {
+        alert('Idade inválida')
+        validarUsuario()
+    }
+
+    let salario = window.prompt(`
         Digite o Salário do usuário:
     `)
-        if (salario > 0) {
-            alert(`Salário igual a ${salario}`)
-        } else {
-            alert('Salário igua a zero')
-            validarUsuario()
-        }
-        
-        let sexo = window.prompt(`
+    if (salario > 0) {
+        alert(`Salário igual a ${salario}`)
+    } else {
+        alert('Salário igua a zero')
+        validarUsuario()
+    }
+
+    let sexo = window.prompt(`
         Digite o Sexo do usuário:
         m para masculino
         f para feminino
     `)
-        if (sexo == 'm') {
-            alert('Sexo Masculino')
-        } else if (sexo == 'f') {
-            alert('Sexo Feminino')
-        } else {
-            alert('opção inválida')
-            validarUsuario()
-        }
+    if (sexo == 'm') {
+        alert('Sexo Masculino')
+    } else if (sexo == 'f') {
+        alert('Sexo Feminino')
+    } else {
+        alert('opção inválida')
+        validarUsuario()
+    }
 
-        let estadoCivil = window.prompt(`
+    let estadoCivil = window.prompt(`
         Digite o Estado Civil do usuário:
         s para solteiro
         c para casado
         v para viúvo
         d para divorciado
     `)
-        if (estadoCivil == 's') {
-            alert('Solteiro')
-        } else if (estadoCivil == 'c') {
-            alert('Casado')
-        } else if (estadoCivil == 'v') {
-            alert('Viúvo')
-        } else if (estadoCivil == 'd') {
-            alert('Divorciado')
-        } else {
-            alert('opção inválida')
-            validarUsuario()
-        }
-
-
+    if (estadoCivil == 's') {
+        alert('Solteiro')
+    } else if (estadoCivil == 'c') {
+        alert('Casado')
+    } else if (estadoCivil == 'v') {
+        alert('Viúvo')
+    } else if (estadoCivil == 'd') {
+        alert('Divorciado')
+    } else {
+        alert('opção inválida')
+        validarUsuario()
     }
-    validarUsuario() 
+
+
+}
+validarUsuario() 
