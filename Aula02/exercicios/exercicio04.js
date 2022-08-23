9713d729aae757475d416975fec16e2bbf9af039
gilberto barbato <gibarbato@gmail.com>
2022-08-23 19:02:29 -0300

    exercício 04 - finalizado

--- Aula02/exercicios/exercicio04.js
+++ Aula02/exercicios/exercicio04.js
@@ -5,22 +5,40 @@ mantidas as taxas de crescimento.
 Altere o programa anterior permitindo ao usuário informar as populações e as taxas de crescimento iniciais. 
 Valide a entrada e permita repetir a operação. */
 
-let popA = 80000;
-let popAtualA = 0;
-let popB = 200000;
-let popAtualB = 0;
+/* let popA = window.prompt(`Digite a população da cidade A:`)
+let popB = window.prompt(`Digite a população da cidade B:`)
+let taxaA = window.prompt(`Digite a taxa de crescimento população da cidade A:
+                            usar (.) ponto no lugar da (,) virgula `)
+let taxaB = window.prompt(`Digite a taxa de crescimento população da cidade B:
+                            usar (.) ponto no lugar da (,) virgula`)
+
+let anos = 0;
+let crescA = (popA * taxaA)/100;
+let crescB = (popB * taxaB)/100;
+
+alert(`População de A = ${popA}  População de B = ${popB}`);
+alert(`Crescimento A = ${crescA}  Crescimento B = ${crescB}`); */
+
+
+let popA = window.prompt(`insira a população da cidade A`);
+let popB = window.prompt(`insira a população da cidade B`);
+let txA = window.prompt(`Digite a taxa de crescimento população da cidade A:
+                            usar (.) ponto no lugar da (,) virgula `)
+let txB = window.prompt(`Digite a taxa de crescimento população da cidade B:
+                            usar (.) ponto no lugar da (,) virgula`)
+
+let popAtualA = parseInt(popA);
+let popAtualB = parseInt(popB);
+txA = parseFloat(txA / 100);
+txB = parseFloat(txB / 100);
 let anos = 0;
 let crescA = 0;
 
-let txA = 0.03;
-let txB = 0.015;
 
-popAtualB = popB;
-
-for (popAtualA = popA; popAtualA <= popAtualB; crescA = popAtualA * txA) {
+for (popAtualA; popAtualA <= popAtualB; crescA = popAtualA * txA) {
     popAtualA += crescA;
     popAtualB = popAtualB + (popAtualB * txB)
     anos++;
-    document.writeln(`crescimento populacional de A = ${popAtualA.toFixed(0)}  de B = ${popAtualB.toFixed(0)} - anos = ${anos}<br>`);
+    //document.writeln(`crescimento populacional de A = ${popAtualA.toFixed(0)}  de B = ${popAtualB.toFixed(0)} - anos = ${anos}<br>`);
 }
 alert(`crescimento populacional de A = ${popAtualA.toFixed(0)}  de B = ${popAtualB.toFixed(0)} - anos = ${anos}`);
