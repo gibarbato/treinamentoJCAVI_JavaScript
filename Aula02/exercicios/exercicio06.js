0d9ebe8d1473be3f8a9e9d3384330a92fc60d283
gilberto barbato <gibarbato@gmail.com>
2022-08-24 10:10:27 -0300

    finalizando exercício 065

---
+++ Aula02/exercicios/exercicio06.js
@@ -0,0 +1,22 @@
+/* 6 - Faça um programa que leia 5 números e informe o maior número. */
+
+function maiorNumero() {
+
+    let limite = window.prompt('digite a quantidadede números para o teste')
+    let maior = 0;
+    let valorRecebido = 0;
+
+    for (i = 1; i <= limite; i++) {
+        valorRecebido = window.prompt(`${i}º valor:`);
+
+        document.writeln(`${i}º número = ${valorRecebido} <br>`);
+
+        if (valorRecebido > maior) {
+            maior = valorRecebido;
+        }
+
+    }
+    document.writeln(`O maior valor digitado da comparação de ${i-1} números foi --> ${maior}`);
+}
+
+maiorNumero();
