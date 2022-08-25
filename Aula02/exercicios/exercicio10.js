ad7b1b9205084bda42c196b1883cdcfc738a6f12
gilberto barbato <gibarbato@gmail.com>
2022-08-25 08:05:40 -0300

    exercício 10 alterado para mostrar a soma dos valores do intervalo

--- Aula02/exercicios/exercicio10.js
+++ Aula02/exercicios/exercicio10.js
@@ -1,25 +1,27 @@
 //10 - Faça um programa que receba dois números inteiros e gere os números inteiros 
 //que estão no intervalo compreendido por eles.
 
-function intervalo(){
+function intervalo() {
     let valorInicial = window.prompt(`insira o valor inicial:`);
-    let valorFinal =  window.prompt(`insira o valor final:`);
+    let valorFinal = window.prompt(`insira o valor final:`);
     let valoresIntervalo = 0;
-    
-    valorInicial=parseInt(valorInicial);
-    valorFinal=parseInt(valorFinal);
+    let somaValores = 0;
+
+    valorInicial = parseInt(valorInicial);
+    valorFinal = parseInt(valorFinal);
 
     if (valorFinal <= valorInicial) {
-    alert(`O valor final deve ser maior que o valor inicial`);
-    intervalo()
+        alert(`O valor final deve ser maior que o valor inicial`);
+        intervalo()
     }
-    
+
     //valorInicial = parseInt(valorInicial);
     //valorFinal = parseInt(valorFinal);
-    for(valoresIntervalo = valorInicial; valoresIntervalo <= valorFinal; valoresIntervalo++) {
-    
-    // valoresIntervalo+=1;
-    document.writeln(`${valoresIntervalo}`);
+    for (valoresIntervalo = valorInicial; valoresIntervalo <= valorFinal; valoresIntervalo++) {
+
+        somaValores = somaValores + valoresIntervalo;
+        document.writeln(`${valoresIntervalo}`);
     }
-    }
-    intervalo()
+    document.writeln(`<br> Somo dos valores do intervalo é :${somaValores}`);
+}
+intervalo()
