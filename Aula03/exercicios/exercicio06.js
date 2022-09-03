4adfaf4f0b2d1316c4ca52d19ccaa8d48801c290
gilberto barbato <gibarbato@gmail.com>
2022-09-03 10:12:51 -0300

    alterando nomne da variável soma para somar no exercício 06

--- Aula03/exercicios/exercicio06.js
+++ Aula03/exercicios/exercicio06.js
@@ -13,26 +13,26 @@ elemento “c”. Então, crie uma função que retornar este elemento. */
 
 const vetores = ["a", "b", "a", "a", "c", "b"]
 let count = 0;
-let soma = 0
+let somar = 0
 let resposta
 
-
+console.log('antes da function')
 function retornaElementoUnitario() {
     for (let x = 0; x < vetores.length; x++) {
         for (let y = 0; y < vetores.length; y++) {
             if (vetores[x] === vetores[y]) {
                 count++
-                soma += count
+                somar += count
             }
 
-            document.writeln(`(${vetores[x]}, ${vetores[y]} - contador ${count} - soma ${soma})<br>`)
+            document.writeln(`(${vetores[x]}, ${vetores[y]} - contador ${count} - soma ${somar})<br>`)
             count = 0;
 
         }
-        if (soma === 1) {
+        if (somar === 1) {
             resposta = vetores[x]
         }
-        soma = 0;
+        somar = 0;
     }
     document.writeln(`Elemento que não possui repetição ${resposta}`)
 
